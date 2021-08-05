f7f6285d63c21ba2c843d2f4a2bf7f524fd31426
HelenTR7 <strelchenya-91@mail.ru>
2021-08-05 20:46:56 +0300

    show error

--- gulp-tasks/pug.js
+++ gulp-tasks/pug.js
@@ -8,6 +8,7 @@ module.exports = function (gulp, options, plugins) {
 			.pipe(plugins.pug({
 				pretty: true
 			}))
+			.pipe(plugins.plumber.stop())
 			.pipe(gulp.dest(options.path.build.html))
 			.on('end', plugins.browserSync.reload);
 		cb();
